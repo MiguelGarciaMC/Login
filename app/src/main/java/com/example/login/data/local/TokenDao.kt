7adcf413113a321d70f1134b1cb909060a985131
MiguelGarciaMC <miguel.garcia@mccollect.mx>
2025-10-06 10:10:47 -0600

    con sqlite directo

--- app/src/main/java/com/example/login/data/local/TokenDao.kt
+++ app/src/main/java/com/example/login/data/local/TokenDao.kt
@@ -1,45 +1,9 @@
 package com.example.login.data.local
+import com.example.login.data.local.TokenEntity
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
-
-@Dao //Data Access Object
 interface TokenDao {
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsert(token: TokenEntity) //ingresa o remplaza
-
-    //Se observa toda la fila (token + firstname)
-    @Query("SELECT token FROM token WHERE id = 1 LIMIT 1")
-    fun observeToken(): Flow<String?> // observa y s cambia devuelve un flow que emite el valor del token
-
-    //Lectura puntual
-    @Query("SELECT token FROM token WHERE id = 1 LIMIT 1")
-    suspend fun getTokenOnce(): String?
-
-    @Query("DELETE FROM token WHERE id = 1")
-    suspend fun clear()
-
-    //Recupera el fistName
-    @Query("SELECT * FROM token WHERE id = 1")
-    suspend fun getUser(): TokenEntity?
+    fun upsert(token: TokenEntity)
+    fun getTokenOnce(): String?
+    fun getUser(): TokenEntity?
+    fun clear()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//hola soy un comentario
--- app/src/main/java/com/example/login/data/local/TokenEntity.kt
+++ app/src/main/java/com/example/login/data/local/TokenEntity.kt
@@ -1,11 +1,7 @@
 package com.example.login.data.local
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "token")
 data class TokenEntity(
-    @PrimaryKey val id: Int = 1,
+    val id: Int = 1,
     val token: String,
     val firstName: String
 )
